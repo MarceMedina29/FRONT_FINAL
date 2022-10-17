a1768c7f9b87184097aaa07b10aed24a1c70899b
marcela <diana.medina.mt@usa.edu.co>
2022-10-17 09:11:26 -0500

    first commit

---
+++ js/funcionesMachine.js
@@ -0,0 +1,118 @@
+//GET, POST , PUT Y DELETE
+
+function getMachine (){
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Machine/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            
+            pintarMachine(respuesta);
+        }
+    });
+
+}
+
+function postMachine(){
+    let cajas = {
+        name:$("#name").val(),
+        brand:$("#brand").val(),
+        year:$("#year").val(),
+        description:$("#description").val(),
+        category:{id: +$("#select-categoria").val()}
+    };
+    console.log(cajas);
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Machine/save",
+        type:"POST",
+        datatype:"JSON",
+        contentType:"application/json; charset=utf-8",
+        data: JSON.stringify(cajas),
+        success:function(respuesta){
+            alert("se creo correctamente la maquina");
+            window.location.reload();
+    
+        }
+    });
+
+}
+
+function putMachine(idDesdeBoton){
+    console.log(idDesdeBoton);
+    if($("#name").val().length==0 || $("#brand").val().length==0 || $("#year").val().length==0 || $("#description").val().length==0 ){
+        alert("Todos los campos son obligatorios");
+    }else{
+    let cajas = {
+        name:$("#name").val(),
+        brand:$("#brand").val(),
+        year:$("#year").val(),
+        description:$("#description").val(),
+        category:{id: +$("#select-categoria").val()}
+    };
+    console.log(cajas);
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Category/update",
+        type:"PUT",
+        datatype:"JSON",
+        contentType:"application/json",
+        data: JSON.stringify(cajas),
+        success:function(respuesta){
+            alert("Se actualizo correctamente la correctamente la maquina");
+            window.location.reload();
+        }
+    });
+    }
+}
+
+function deleteMachine(idDesdeBoton){
+    let myData={
+        id:idDesdeBoton
+    };
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Category/"+idDesdeBoton,
+        type:"DELETE",
+        datatype:"JSON",
+        data: JSON.stringify(myData),
+        contentType:"application/json",
+        success:function(respuesta){
+            alert("Se borro correctamente la maquina");
+            window.location.reload();
+        }
+    });
+
+}
+
+////////////////////////////////////////////////////////
+
+function pintarMachine(json_maquinas){
+    let myTable='<table class="table-auto w-full table-center text-center whitespace-w-48">';
+    for(i=0;i<json_maquinas.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+json_maquinas[i].name+"</td>";
+        myTable+="<td>"+json_maquinas[i].brand+"</td>";
+        myTable+="<td>"+json_maquinas[i].year+"</td>";
+        myTable+="<td>"+json_maquinas[i].description+"</td>";
+        myTable+="<td>"+json_maquinas[i].category.name+"</td>";
+        myTable+="<td> <button class='mx-auto text-white bg-blue-700 border-0 py-1 px-7 focus:outline-none hover:bg-gray-400 rounded text-lg' onclick='putCategoria("+respuesta[i].id+") '> Actualizar</button>"
+        myTable+="<td> <button class='mx-auto text-white bg-blue-700 border-0 py-1 px-7 focus:outline-none hover:bg-gray-400 rounded text-lg' onclick='deleteCategoria("+respuesta[i].id+")'> Borrar</button>"
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultadoMachine").html(myTable);
+
+}
+
+function getCategoria_Machine(){
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            let $select = $("#select-categoria");
+            $.each(respuesta, function(id, name){
+                $select.append('<option value='+name.id+'>'+name.name+'</option>' )
+            })
+        }
+    });
+}
---
+++ js/reportes.js
@@ -0,0 +1,103 @@
+function traerReporteStatus(){
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Reservation/report-status",
+        type: "GET",
+        datatype: "JSON",
+        success:function(respuesta){
+            console.log(respuesta)
+            pintarStatus(respuesta);
+        }
+    });
+}
+
+function pintarStatus(json_maquinas){
+    
+    let myTable = "<table>";
+
+        myTable += "<tr>";
+        myTable += "<td>" + "Reservas completa: " + json_maquinas.completed + "</td>";
+        myTable += "</tr>";
+        myTable += "<tr>";
+        myTable += "<td>" + "Reservas cancelada: " + json_maquinas.cancelled + "</td>";
+        myTable += "</tr>";
+     
+    myTable += "</table>";
+    $("#resultado1").html(myTable);
+}
+
+function traerReportesFechas(){
+    let startDate = $("#startDate").val();
+    let endDate = $("#endDate").val();
+
+    console.log(startDate, endDate)
+
+    $.ajax({
+        url:`http://138.2.233.157:8080/api/Reservation/report-dates/${startDate}/${endDate}`,
+        type: "GET",
+        datatype: "JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarFechas(respuesta);
+        }
+    });
+}
+
+
+function pintarFechas(respuesta){
+
+    let completadas = 0;
+    let canceladas = 0;
+
+    for(let i = 0; i < respuesta.length; i++){
+        if(respuesta[i].status == "cancelled"){
+            canceladas++;
+            console.log(`Reservas canceladas: ${canceladas}`)
+        }else if(respuesta[i].status == "completed"){
+            completadas++;
+            console.log(`Reservas completas: ${completadas}`)
+        }
+    }
+
+    let myTable = "<table>";
+
+        myTable += "<tr>";
+        myTable += "<td>" + "Reservas completas: " + completadas + "</td>";
+        myTable += "</tr>";
+        myTable += "<tr>";
+        myTable += "<td>" + "Reservas canceladas: " + canceladas + "</td>";
+        myTable += "</tr>";
+    
+        
+    myTable += "</table>";
+    $("#resultado2").html(myTable);
+}
+
+function traerReportesClientes(){
+    $.ajax({
+        url:"http://138.2.233.157:8080/api/Reservation/report-clients",
+        type: "GET",
+        datatype: "JSON",
+        success:function(respuesta){
+            console.log(respuesta)
+            pintarClientes(respuesta);
+        }
+    });
+}
+
+function pintarClientes(respuesta){
+
+    let myTable = "<table class='max-w-2xl'>";
+    for(let i = 0; i < respuesta.length; i++){
+        myTable += "<tr>";
+        myTable += `<td>ID: +${respuesta[i].idClient}+ </td>`;
+        myTable += `<td>Nombre:+${respuesta[i].name}+ </td>`;
+        myTable += `<td>Email:+${respuesta[i].email}+ </td>`;
+        myTable += `<td>Contraseña: +${respuesta[i].password}+ </td>`;
+        myTable += `<td>Edad: +${respuesta[i].age}, </td>`;
+        myTable += `<td>Reservas totales: +${respuesta[i].total} +</td>`;
+        myTable += "</tr>";
+    } 
+    myTable += "</table>";
+    $("#resultado3").html(myTable);
+
+}
